680fe5963f2fe1644f378450880a2e31146374e3
unknown <alexander.j.schaefer@uvm.edu>
2026-01-19 15:30:54 -0500

    Final commit

--- generate.py
+++ generate.py
@@ -14,7 +14,28 @@ length = 1
 width = 1
 height = 1
 
+x = 0
+y = 0
+z = height / 2
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[length,width,height])
+scaling_factor = 0.9
+current_scale = 1
+
+for i in range(10):
+    # make a box
+    name = f"Box{i+1}"
+    pyrosim.Send_Cube(name=name, pos=[x,y,z], size=[length,width,height])
+    
+    # update the height of the next box
+    z = z + height/2 + height*scaling_factor/2
+
+    # update the size of the next box
+    length *= scaling_factor
+    width *= scaling_factor
+    height *= scaling_factor
+
+
+    # update current_scale
+    current_scale *= scaling_factor
 
 pyrosim.End()
